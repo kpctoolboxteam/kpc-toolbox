7843013a1b909095e275d7277a026bb795d95b69
Giuliano Casale <g.casale@imperial.ac.uk>
2015-04-14 08:47:55 +0530

    Tree restructured

--- mapqntbx_feastol.m
+++
@@ -1,7 +0,0 @@
-function FEASTOL=mapqntbx_feastol()
-% FEASTOL=mapqntbx_feastol() - tolerance for feasibility (default 1e-12)
-%
-% MAP Queueing Networks Toolbox
-% Version 1.0 	 15-Apr-2008
-FEASTOL=8;
-end
