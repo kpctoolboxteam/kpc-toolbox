25a3df4f2b7fa5e24b78e3f4bc182f3ea4d388b7
Giuliano Casale <g.casale@imperial.ac.uk>
2019-05-22 08:48:02 +0100

    Updated ctmc_solve and trace_acf in corner cases.

--- trace/trace_acf.m
+++ trace/trace_acf.m
@@ -8,23 +8,29 @@ if nargin==1
     lags=1;
 end
 
-
 rho=[];
+
+if max(lags)>length(S)-2
+    warning('Lags are higher than the trace length, truncating the lags vector.');
+    lags = min(lags,length(S)-2);
+    lags(lags == length(S)-2) = [];
+end
 
 if exist('xcorr')
     rho=xcorr(S-mean(S),max(lags),'coeff');
     rho=rho((max(lags)+2):end);
     rho=rho(lags);
 else
-    warning('trace_acf: signal processing toolbox not found, autocorrelation estimation may be slower than usual.');
+    %warning('trace_acf: signal processing toolbox not found, autocorrelation estimation may be slower than usual.');
     S=S(:);
-    tic; acv = autocov(S-mean(S)); rho=acv(1+lags)'/acv(1); toc
-    % E1=mean(S);
-    %     E2=mean(S.^2);
-    %     rho = zeros(1,length(lags));
-    %     for ki=1:length(lags)
-    %         rho(ki)=(trace_joint(S,[0,lags(ki)],[1,1])-E1^2)/(E2-E1^2);
-    %     end
+    acv = autocov(S-mean(S));
+    rho=acv(1+lags)'/acv(1);
+%     E1=mean(S);
+%     E2=mean(S.^2);
+%     rho = zeros(1,length(lags));
+%     for ki=1:length(lags)
+%         rho(ki)=(trace_joint(S,[0,lags(ki)],[1,1])-E1^2)/(E2-E1^2);
+%     end
 end
 
 rho=rho(:);
