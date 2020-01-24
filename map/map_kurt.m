f2a3f608732be6a0854c975dd0ad860c1195d9be
Gabriele Russo Russo <gabri.russo17@gmail.com>
2020-01-24 16:19:43 +0100

    fix: map_kurt() not returning correct value

--- map/map_kurt.m
+++ map/map_kurt.m
@@ -8,8 +8,8 @@ function KURT=map_kurt(MAP)
 %  KURT: kurtosis of the interarrrival times
 %
 %
-
+m=zeros(1,4);
 for i=1:4
     m(i)=map_moment(MAP,i);
 end
-KURT=m(4)-4*m(3)*m(1)+6*m(2)*m(1)^2-3*m(1)^4;
+KURT=(m(4)-4*m(3)*m(1)+6*m(2)*m(1)^2-3*m(1)^4)/map_var(MAP)^2;
