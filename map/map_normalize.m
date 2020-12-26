85a3845aa41f581df7050b24bf7b1e49a5731516
Giuliano Casale <g.casale@imperial.ac.uk>
2020-12-26 12:52:05 +0000

    Fixed map_normalize bug.

--- map/map_normalize.m
+++ map/map_normalize.m
@@ -1,33 +1,29 @@
-function MAP=map_normalize(MAP)
-% MAPOUT=map_normalize(MAPIN) - Try to make a MAP feasible
+function MAP = map_normalize(MAP)
+% MAPOUT = map_normalize(MAPIN) - Try to make a MAP feasible
 %
 %  Input:
 %  MAPIN: a MAP in the form of {D0,D1}
 %
 %  Output:
 %  MAPOUT: MAPIN with D0 normalized to make D0+D1 an infinitesimal
-%  generator, with all negative entries set to zero, and with complex  
+%  generator, with all negative entries set to zero, and with complex
 %  values set equal to their norm
 %
 %  Examples:
 %  - map_normalize({[0,0;0,0],[1,2;3,4]}) produces a valid MAP
 %
 
-D0=MAP{1};
-D1=MAP{2};
-for i=1:size(D1,1)
-    for j=1:size(D1,1)
-        D0(i,j)=real(D0(i,j));
-        D1(i,j)=real(D1(i,j));
-    end
+D0=real(MAP{1});
+D1=real(MAP{2});
+D0 = D0 - diag(diag(D0)); % remove diagonal
+D0(D0<0)=0; % remove invalid entries
+D0 = D0 - diag(sum(D0));
+MAP{1} = D0;
+for b=2:length(MAP) %% D1..Dn
+    Db = MAP{b};
+    Db(Db<0)=0;
+    D0 = D0 - diag(sum(Db,2));
+    MAP{b} = Db;
 end
-D0(find(D0<0))=0;
-D1(find(D1<0))=0;
-for n=1:size(D0,1)
-    D0(n,n)=0;
-    for b=1:length(MAP)
-        D0(n,n)=D0(n,n)-sum(MAP{b}(n,:));
-    end
-end
-MAP = {D0,D1};
+MAP{1} = D0;
 end
