2318aa4d789cd88c6e82fddcf28f53406d364571
Giuliano Casale <g.casale@imperial.ac.uk>
2025-06-12 06:21:44 +0100

    Update trace_skew.m

--- trace/trace_skew.m
+++ trace/trace_skew.m
@@ -2,7 +2,11 @@ function skew = trace_skewness(S)
 % [skew] = trace_skewness(S)
 %
 % DESCRIPTION
-% Compute the skewness of trace S
+% Compute the bias-corrected skewness of trace S (equivalent to skewness(S,0))
 
-skew = mean((S - mean(S)).^3) / std(S)^3;
+n = numel(S);
+m = mean(S);
+s = std(S, 1); % population standard deviation
+
+skew = (n / ((n - 1) * (n - 2))) * sum(((S - m) / s).^3);
 end
